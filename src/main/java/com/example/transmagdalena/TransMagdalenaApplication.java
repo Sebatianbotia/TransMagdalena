f8b27dd897c9b03086d1e4bfa60f85166a7025c9
Sebastián Botia <adrianbotia@gmail.com>
2025-10-30 11:29:50 -0500

    TransMagdalena

---
+++ src/main/java/com/example/transmagdalena/TransMagdalenaApplication.java
@@ -0,0 +1,13 @@
+package com.example.transmagdalena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransMagdalenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransMagdalenaApplication.class, args);
+	}
+
+}
